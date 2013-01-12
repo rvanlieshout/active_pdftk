b6cd70bc7fabd57ebbc5cabc3ed2d854bf1d7ed9
Koen Handekyn <koen.handekyn@up-nxt.com>
2013-01-12 12:53:32 +0100

    MetaDataParser module

---
+++ lib/active_pdftk/metadata.rb
@@ -0,0 +1,63 @@
+require 'date'
+
+module ActivePdftk
+
+  class MetaData < Hash
+
+    def number_of_pages
+      self["NumberOfPages"]
+    end
+
+    def author
+      self["Author"]
+    end
+
+    def mod_date
+      parse_date self["ModDate"]
+    end
+
+    def creation_date
+      parse_date self["CreationDate"]
+    end
+
+    private 
+
+      def parse_date raw
+        key, raw_date = raw.split(":")
+        date = raw_date[0..-4] # strip of '00'
+        DateTime.parse(date)
+      end
+
+  end
+
+  class MetaDataParser
+
+    def initialize
+      @pdftk = Wrapper.new
+    end
+
+    def parse(input)
+      raw_data = @pdftk.dump_data(input)
+      lines = raw_data.read.split("\n")
+      puts lines
+      meta_data = MetaData.new
+      new_key = nil
+      lines.each do |line|
+        key, value = line.split(": ")
+        case key
+          when "InfoBegin"
+          when "InfoKey"
+            new_key = value
+          when "InfoValue"
+            meta_data[new_key] = value
+          else 
+            meta_data[key] = value unless value.nil?
+        end 
+      end
+      puts meta_data
+      meta_data
+    end
+
+  end
+
+end
